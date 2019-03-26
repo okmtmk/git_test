b8fd32edf4c7fba414b74deb1099f3a14483f2f7
okamoto <c0118060b4@edu.teu.ac.jp>
2019-03-26 09:27:32 +0900

    [change]HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello world!!");
+        System.out.println(returnHelloWorld);
+    }
+
+    public static String returnHelloWorld(){
+        return "Hello world!!";
     }
 }
