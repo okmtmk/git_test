1156dec568ca588c4c45318555ccad17ab073e59
okamoto <c0118060b4@edu.teu.ac.jp>
2019-03-26 09:23:25 +0900

    [add]足し算のメソッド追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello world!!");
+        System.out.println(tashizan(4,5));
+    }
+
+    public static int tashizan(int a, int b){
+        return a + b;
     }
 }
